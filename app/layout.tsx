82f9a349a1cee987db8f6d3c61a58c4c08d3ea1a
Anurag Ojha <aojharaj2004@gmail.com>
2025-06-17 22:10:10 +0530

    Analytics of website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 
 import { ThemeProvider } from "@/components/theme-provider"
@@ -30,6 +31,7 @@ export default function RootLayout({
         <ThemeProvider  attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
           <Toaster />
+          <Analytics/>
         </ThemeProvider>
       </body>
     </html>
